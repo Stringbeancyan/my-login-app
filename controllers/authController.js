2ba4e58bbcf72f3e6c4dffdb26ac921d4b306e89
Stringbeancyan <sgfriss@reagan.com>
2024-10-01 11:34:48 -0400

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,22 @@
-// controllers/authController.js
 const db = require('../config/db');
 
 exports.login = (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
-        if (err) {
-            return res.status(500).send('Database error');
-        }
-        if (row) {
-            req.session.user = { username: row.username, role: 'admin' };
-            return res.redirect('/admin'); // Redirect to admin page
-        }
-        res.status(401).send('Invalid credentials');
-    });
+  db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+    if (err) {
+      return res.status(500).send('Database error');
+    }
+
+    if (!row) {
+      return res.status(401).send('Login failed!');
+    }
+
+    // Redirect to admin panel if admin
+    if (username === 'Stryngbean_cyan' && password === 'nathaniel2010') {
+      return res.redirect('https://admin-panel-app.vercel.app/');
+    } else {
+      return res.send('Login successful!');
+    }
+  });
 };
-
